3f2664e59d8385230d86e8fa4a86e6ef560bea6f
rmisutyte <ruta.mi@protonmail.com>
2024-06-07 10:09:17 +0300

    aoc 1, 2 and 3 part 1

---
+++ 3/3-part1.ts
@@ -0,0 +1,52 @@
+import { input } from "./3.input.js";
+
+const getNumbers = (input: string) => {
+  let numbers: { number: string; start: number; end: number }[] = [];
+  const numbersInInput = input.matchAll(/\d+/g);
+
+  for (let number of numbersInInput) {
+    numbers.push({
+      number: number[0],
+      start: number.index!,
+      end: number.index! + number[0].length - 1,
+    });
+  }
+
+  return numbers;
+};
+
+const isSymbol = (character: string) => {
+  const reg = new RegExp(/[^\d.]/g);
+  return character ? reg.test(character) : false;
+};
+
+const getSum = (input: string[]) => {
+  let sum = 0;
+
+  input.forEach((line: string, lineIndex: number) => {
+    const numbers = getNumbers(line);
+
+    numbers.forEach((number) => {
+      // check same line
+      if (isSymbol(line[number.start - 1]) || isSymbol(line[number.end + 1])) {
+        sum += Number(number.number);
+      }
+      // check prev line
+      for (let i = number.start - 1; i <= number.end + 1; i++) {
+        if (input[lineIndex - 1] && isSymbol(input[lineIndex + -1][i])) {
+          sum += Number(number.number);
+        }
+      }
+      // check next line
+      for (let i = number.start - 1; i <= number.end + 1; i++) {
+        if (input[lineIndex + 1] && isSymbol(input[lineIndex + 1][i])) {
+          sum += Number(number.number);
+        }
+      }
+    });
+  });
+
+  return sum;
+};
+
+console.log(getSum(input));
