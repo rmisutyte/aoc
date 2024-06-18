95f36680d55677116a057183b58e241a73e85e9f
rmisutyte <ruta.mi@protonmail.com>
2024-06-18 15:49:13 +0300

    proper formatting

--- 3/part-2/index.ts
+++ 3/part-2/index.ts
@@ -1,9 +1,9 @@
 import { input } from "./input";
-type NumberData = { 
+type NumberData = {
   number: number;
   start: number;
-  end: number 
-}
+  end: number;
+};
 
 const getNumbers = (input: string) => {
   let numbers: NumberData[] = [];
@@ -16,52 +16,71 @@ const getNumbers = (input: string) => {
     });
   }
   return numbers;
-}
+};
 
 const getAsteriskIndexes = (input: string) => {
-  return [...input.matchAll(/\*/g)].map((asterisk) => {return asterisk.index})
-}
+  return [...input.matchAll(/\*/g)].map((asterisk) => {
+    return asterisk.index;
+  });
+};
 
-const getAdjacentNumbersForAsterisk = (asteriskIndex: number, numbers: NumberData[]) => {
+const getAdjacentNumbersForAsterisk = (
+  asteriskIndex: number,
+  numbers: NumberData[]
+) => {
   return numbers.filter((number) => {
-    for(let i = number.start-1;i < number.end+2; i++) {
+    for (let i = number.start - 1; i < number.end + 2; i++) {
       if (asteriskIndex === i) {
         return true;
       }
     }
     return false;
-  })
-}
+  });
+};
 
 const getSumOfGearRations = (input: string[]) => {
+  const gearRations = input
+    .map((line, lineIndex) => {
+      const asterisksAt = getAsteriskIndexes(line);
 
-  const gearRations = input.map((line, lineIndex) => {
-    const asterisksAt = getAsteriskIndexes(line);
+      if (asterisksAt.length > 0) {
+        const adjacentNumbersForAsterisk = asterisksAt.map((asteriskIndex) => {
+          const allSurroundingNumbers = [
+            input[lineIndex - 1],
+            line,
+            input[lineIndex + 1],
+          ]
+            .filter((line) => line !== undefined)
+            .map((line) => {
+              return getNumbers(line);
+            })
+            .filter((numbers) => numbers.length > 0)
+            .flat();
 
-    if (asterisksAt.length > 0) {
-      const adjacentNumbersForAsterisk = asterisksAt.map((asteriskIndex) => {
-        const allSurroundingNumbers = [input[lineIndex-1], line, input[lineIndex+1]].filter((line) => line !== undefined).map((line) => {
-          return getNumbers(line)
-        }).filter((numbers) => numbers.length > 0).flat()
+          const adjacentNumbers = getAdjacentNumbersForAsterisk(
+            asteriskIndex!,
+            allSurroundingNumbers
+          );
+          return adjacentNumbers;
+        });
 
-      const adjacentNumbers = getAdjacentNumbersForAsterisk(asteriskIndex!, allSurroundingNumbers)
-      return adjacentNumbers;
-      })
-
-      return adjacentNumbersForAsterisk;
-    } else {
-      return undefined;
-    }
-    }).filter(el => el !== undefined).flat().filter((el) => {
-      return el!.length === 2;
+        return adjacentNumbersForAsterisk;
+      } else {
+        return undefined;
+      }
     })
+    .filter((el) => el !== undefined)
+    .flat()
+    .filter((el) => {
+      return el!.length === 2;
+    });
 
-    const sum = gearRations.reduce((acc, current) => {
-      acc += (current![0].number * current![1].number)
-      return acc;
-    }, 0)
-  
+  const sum = gearRations.reduce((acc, current) => {
+    acc += current![0].number * current![1].number;
+    return acc;
+  }, 0);
+
   return sum;
-}
+};
 
-console.log(getSumOfGearRations(input))
+console.log(getSumOfGearRations(input));
